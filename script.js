df3f5ba266a214372fcabad5e814ffeedfea2549
shawal01 <shawalnoor@gmail.com>
2026-02-18 00:05:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,31 +4,30 @@ const response = document.getElementById("response");
 
 const canvas = document.getElementById("fireworks");
 const ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-noBtn.addEventListener("click", () => {
+noBtn.addEventListener("click", function () {
   response.innerText = "That's not an option 😌❤️";
 });
 
-yesBtn.addEventListener("click", () => {
+yesBtn.addEventListener("click", function () {
   response.innerText = "YAAAY!!! I love you 💖💖💖";
   startFireworks();
 });
 
 function startFireworks() {
-  for (let i = 0; i < 200; i++) {
-    drawFirework();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < 300; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const r = Math.random() * 3 + 1;
+
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2);
+    ctx.fillStyle = "hsl(" + Math.random() * 360 + ",100%,60%)";
+    ctx.fill();
   }
-}
-
-function drawFirework() {
-  const x = Math.random() * canvas.width;
-  const y = Math.random() * canvas.height;
-  const radius = Math.random() * 3 + 1;
-
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.fillStyle = hsl(${Math.random() * 360}, 100%, 60%);
-  ctx.fill();
 }
